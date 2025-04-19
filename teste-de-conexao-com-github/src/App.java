9d92572360bc4f77feb77a294e438a431a1e43b4
Michael leandro <smichaelleandro@gmail.com>
2025-04-18 21:57:41 -0300

    teste 01

--- teste-de-conexao-com-github/src/App.java
+++ teste-de-conexao-com-github/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Teste de conexao com github");
+        System.out.println("Teste de conexao com github!");
     }
 }
